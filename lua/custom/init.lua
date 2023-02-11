691a722c0ae15127ee4129fbc09b5890d48e88ca
DevAles <devalesoficial@gmail.com>
2023-02-11 00:28:24 -0300

    Add trouble source

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,6 @@
 require('custom.remap')
 require('custom.set')
 
--- Source packer
+-- Source packer and remap
 vim.cmd(":so ~/.config/nvim/lua/custom/packer.lua")
+vim.cmd(":so ~/.config/nvim/lua/custom/remap.lua")
