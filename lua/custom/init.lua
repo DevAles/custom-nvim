cfdb98b683d5fdb96d124db78a577cea9092ae79
DevAles <devalesoficial@gmail.com>
2023-02-19 11:40:19 -0300

    Add config to format javascript/typescript files

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,9 +10,24 @@ if vim.fn.argc() == 0 then
     vim.cmd("autocmd VimEnter * :Ex")
 end
 
+vim.g.neoformat_enabled = {
+  c = {'clangformat'},
+  cpp = {'clangformat'},
+  rs = {'rustfmt'},
+  typescript = {'prettier'},
+  javascript = {'prettier'},
+  typescriptreact = {'prettier'},
+  javascriptreact = {'prettier'},
+}
+
+-- Set up an autocommand to format the buffer on write
 vim.cmd([[
   augroup Format
     autocmd!
-    autocmd BufWritePre * undojoin | Neoformat
+    autocmd BufWritePre *.c,*.cpp,*.rs undojoin | Neoformat
   augroup END
 ]])
+
+-- Set up coc-prettier to format TypeScript and JavaScript files
+vim.api.nvim_command('autocmd BufWritePre *.ts,*.tsx,*.js,*.jsx :call CocAction("format")')
+
