b528764b06f2fd75fee6743dd1c0c9a8c3a71e55
DevAles <devalesoficial@gmail.com>
2023-03-11 17:01:46 -0300

    Add which-key config

---
+++ after/plugin/whickkey.lua
@@ -0,0 +1,52 @@
+local wk = require("which-key")
+
+wk.register({
+    a = {
+        name = "Action",
+        a = "Action Selected",
+        c = "Code Action",
+        s = "Source Action",
+    },
+    c = {
+        name = "List",
+        c = "List Commands",
+        d = "List Diagnostics",
+        e = "List Extensions",
+        l = "Codelens Action",
+    },
+    f = {
+        name = "Format",
+        f = "Format Selected"
+    },
+    j = {
+        name = "CocJump",
+        j = "CocNext",
+        k = "CocPrev"
+    },
+    p = {
+        name = "Project",
+        f = { "<cmd>Telescope find_files<cr>", "Find File" },
+        p = "List Resume",
+        s = "Search in Files",
+        v = "Virtual File Browser"
+    },
+    q = {
+        name = "QuickFix",
+        f = "CocFixCurrent"
+    },
+    r = {
+        name = "Refactor",
+        e = "CodeAction Refactor",
+        n = "Rename",
+        r = "CodeAction Refactor Selected",
+    },
+    s = {
+        name = "String",
+        s = "Substitute",
+        y = "List Symbols"
+    },
+    x = {
+        name = "Shell",
+        x = "Set File as Executable"
+    }
+}, { prefix = "<leader>" })
